bfbc464937aa512a6d79eea6d817dd4cf619f364
Robin Norwood <robin.norwood@gmail.com>
2016-05-26 10:08:52 -0500

    Add spotlight exercise.

---
+++ week6/ex_spotlight.py
@@ -0,0 +1,28 @@
+## 1. Write a function that, given a string and an index, prints the character at that index.
+#
+# def illuminate(source, idx=0):
+# ...
+# illuminate('Hollywood', 1)) # prints 'o'
+
+## 2. Update your function so that it prints "index" number of spaces, then the character at that index.
+#
+# def illuminate(source, idx=0):
+# ...
+# illuminate('Hollywood', 4) # prints '    y'
+
+## 3. Write function that, given a string, prints each letter, with spaces in front matching its position in the string.
+#
+# def spotlight(source):
+# ...
+# spotlight('Hollywood') # prints:
+# 'H'
+# ' o'
+# '  l'
+# ...etc...
+
+## 4. Update your function from step #3 to `import time` and sleep for 1/4 second after each letter.
+#
+# def spotlight(source, delay=.25):
+
+## 5. Update your function from step #4 to `import os` and then use `os.system('clear')` to clear the screen after each letter.
+## 6. Write a function that uses the one from step #5 to "sweep" back and forth across the string, like a spotlight.
